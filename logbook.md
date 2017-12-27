1af2621474b48169f2b2cf32627f0bc0f9e0857d
Arvin C <arvin.chireh@gmail.com>
2017-12-27 17:02:21 +0100

    save records in list and iterate multiple times

--- logbook.md
+++ logbook.md
@@ -14,3 +14,11 @@ gunzip *
 # 2017-12-24 Add statistics and plots
 * See trimming1.py.
 * Script now returns some general statistics and histograms, can be used to test effects of algorithm.
+
+# 2017-12-25 Ideas for algorithm
+* Break after the quality has been under x SD in two consecutive calls?
+* Break when the remaining bases have low wuality (i.e. under x SD of mean)?
+* I have created a framework for this in the script, but don't understand how to trim the bases from the records.
+
+# 2017-12-27 Re-shape the program - iterate twice
+* Originally, I intended to parse through the fastq file once and do the trimming instantly. However, I now want to iterate once first to get some statistics, i.e. mean and sd, which I will use for the subsequent trimming (trimming will require additional iteration). Therefore, I decided to store all records in a list first, and then iterate over the list as many times as necessary.
--- scripts/trimming1.py
+++ scripts/trimming1.py
@@ -7,44 +7,73 @@ import numpy
 
 # Trim and Store
 trimmed_sequences = []
-seq_lengths = []
-seq_qualities = []
+
 with open('/Users/arvin/Box Sync/Kurser/2017_Applied.bioinformatics/Project/data/fastq/miseq1.fq', "rU") as fq:
-    for record in SeqIO.parse(fq, "fastq"):  # Parse through each accession
-        for i in range(0, len(record.seq)):  # Create iterator over each base call
-            if numpy.mean(record.letter_annotations["phred_quality"][i:]) < 14 or i == len(record.seq):
-                import pdb; pdb.set_trace()
-                trimmed_sequences.append(record[0:i])
-                break  # stop the loop
+    records = list(SeqIO.parse(fq, "fastq"))
+
+# Get statistics
+untrimmed_seq_lengths = []
+untrimmed_seq_qualities = []
+for record in records:
+    untrimmed_seq_lengths.append(len(record.seq))
+    untrimmed_seq_qualities.extend(record.letter_annotations["phred_quality"])
+
+# Create quality threshold
+threshold = numpy.mean(untrimmed_seq_qualities) - numpy.std(untrimmed_seq_qualities)
+
+# Do trimming
+for record in records:  # Parse through each accession
+    for i in range(0, len(record.seq)):  # Create iterator over each base call
+        if numpy.mean(record.letter_annotations["phred_quality"][i:]) < threshold or i == (len(record.seq)-1):
+            # If condition is met OR end of sequence
+            # import pdb; pdb.set_trace()
+            trimmed_sequences.append(record[0:i])
+            break  # stop the loop
+import pdb; pdb.set_trace()
 
 SeqIO.write(trimmed_sequences, "trimmed_seqs.fasta", "fasta")  ## write to file
 
-        seq_lengths.append(len(record.seq))
-        seq_qualities.extend(record.letter_annotations["phred_quality"])
-        # I use extend instead of append because ojbect is already list
-
-
-SeqIO.write(trimmed_sequences, "trimmed_seqs.fasta", "fasta")  ## write to file
+# Get statistics after trimming
+trimmed_seq_lengths = []
+trimmed_seq_qualities = []
+for record in trimmed_sequences:
+    trimmed_seq_lengths.append(len(record.seq))
+    trimmed_seq_qualities.extend(record.letter_annotations["phred_quality"])
+    # I use extend instead of append because ojbect is already list
 
 # Statistics
-print(numpy.mean(seq_lengths))
-print(numpy.mean(seq_qualities))
-print("max quality:", (max(seq_qualities)))
-print("min quality:", (min(seq_qualities)))
-numpy.std(seq_qualities)
+print("untrimmed mean seq length;", numpy.mean(untrimmed_seq_lengths))
+print("trimmed mean seq length:", numpy.mean(trimmed_seq_lengths))
+print("untrimmed mean quality:", numpy.mean(untrimmed_seq_qualities))
+print("trimmed mean quality:", numpy.mean(trimmed_seq_qualities))
+#print("max quality:", (max(seq_qualities)))
+#print("min quality:", (min(seq_qualities)))
 
-# Plot Length
+# Plots
 plt.figure(1)
-plt.hist(seq_lengths)
-plt.title("Seq length histogram")
+plt.hist(untrimmed_seq_lengths)
+plt.title("Untrimmed Seq length histogram")
 plt.xlabel("Value")
 plt.ylabel("Frequency")
-plt.savefig("../results/seqlength.pdf")
+plt.savefig("../results/untrimmed_seqlength.pdf")
 
-# Plot Quality
 plt.figure(2)
-plt.hist(seq_qualities)
-plt.title("Seq quality histogram")
+plt.hist(untrimmed_seq_qualities)
+plt.title("Untrimmed Seq quality histogram")
 plt.xlabel("Value")
 plt.ylabel("Frequency")
-plt.savefig("../results/seqquality.pdf")
+plt.savefig("../results/untrimmed_seqquality.pdf")
+
+plt.figure(3)
+plt.hist(trimmed_seq_lengths)
+plt.title("Trimmed Seq length histogram")
+plt.xlabel("Value")
+plt.ylabel("Frequency")
+plt.savefig("../results/trimmed_seqlength.pdf")
+
+plt.figure(4)
+plt.hist(trimmed_seq_qualities)
+plt.title("Trimmed Seq quality histogram")
+plt.xlabel("Value")
+plt.ylabel("Frequency")
+plt.savefig("../results/trimmed_seqquality.pdf")
