1792e31de29cc5f8be00c188b73639a7e0800551
Arvin C <arvin.chireh@gmail.com>
2017-12-27 18:20:44 +0100

    various improvements

--- logbook.md
+++ logbook.md
@@ -16,9 +16,32 @@ gunzip *
 * Script now returns some general statistics and histograms, can be used to test effects of algorithm.
 
 # 2017-12-25 Ideas for algorithm
-* Break after the quality has been under x SD in two consecutive calls?
-* Break when the remaining bases have low wuality (i.e. under x SD of mean)?
-* I have created a framework for this in the script, but don't understand how to trim the bases from the records.
+* Break after the quality has been under 1 SD in two consecutive calls?
+* Break when the remaining bases have low quality (i.e. under x SD of mean)?
 
 # 2017-12-27 Re-shape the program - iterate twice
 * Originally, I intended to parse through the fastq file once and do the trimming instantly. However, I now want to iterate once first to get some statistics, i.e. mean and sd, which I will use for the subsequent trimming (trimming will require additional iteration). Therefore, I decided to store all records in a list first, and then iterate over the list as many times as necessary.
+
+# 2017-12-27 Evaluation of first algorithm on test and real data
+* My impression is that the current algorithm trims many reads but improves quality only marginally.
+
+## Miseq1
+Nr of seqs trimmed: 22
+Nr of seqs not_trimmed: 3
+Mean seq length before trimming: 245.8
+Mean seq length after trimming: 176.48
+Mean quality before trimming: 17.1938161107
+Mean quality after trimming: 18.2554397099
+
+## Miseq2
+Nr of seqs trimmed: 121838
+Nr of seqs not_trimmed: 128162
+Mean seq length before trimming: 168.813372
+Mean seq length after trimming: 101.328744
+Mean quality before trimming: 29.3656623126
+Mean quality after trimming: 34.9965331061
+
+# 2017-12-27 Improve speed TO-DO
+* Remove one iteration (baked it in previous iteration) to make program faster.
+
+# 2017-12-27 Use argparse to simplify usage
--- scripts/trimming1.py
+++ scripts/trimming1.py
@@ -1,17 +1,29 @@
 #! /usr/bin/env python
 
-import sys
+import argparse
 from Bio import SeqIO
 import matplotlib.pyplot as plt
 import numpy
+import os
+
+dir="yeah/brah"
+file_name = "test.pdf"
+os.path.join(dir, file_name)
+
+parser = argparse.ArgumentParser(description='takes fastq files as input, \
+                                 trims reads, gives statistics and plots')
+parser.add_argument('fastq_file',
+                    help='path to fastq file, e.g. "miseq1.fq"')
+parser.add_argument('output_dir',
+                    help='Save plots to this directory, e.g. "./Results"')
+args = parser.parse_args()
 
 # Trim and Store
 trimmed_sequences = []
-
-with open('/Users/arvin/Box Sync/Kurser/2017_Applied.bioinformatics/Project/data/fastq/miseq1.fq', "rU") as fq:
+with open(args.fastq_file, "rU") as fq:
     records = list(SeqIO.parse(fq, "fastq"))
 
-# Get statistics
+# Get length and quality
 untrimmed_seq_lengths = []
 untrimmed_seq_qualities = []
 for record in records:
@@ -19,19 +31,23 @@ for record in records:
     untrimmed_seq_qualities.extend(record.letter_annotations["phred_quality"])
 
 # Create quality threshold
-threshold = numpy.mean(untrimmed_seq_qualities) - numpy.std(untrimmed_seq_qualities)
+threshold = numpy.mean(untrimmed_seq_qualities) - (numpy.std(untrimmed_seq_qualities)*0.5)
 
 # Do trimming
+trimmed = 0  # for keeping counts
+not_trimmed = 0  # for keeping counts
 for record in records:  # Parse through each accession
     for i in range(0, len(record.seq)):  # Create iterator over each base call
-        if numpy.mean(record.letter_annotations["phred_quality"][i:]) < threshold or i == (len(record.seq)-1):
-            # If condition is met OR end of sequence
+        if numpy.mean(record.letter_annotations["phred_quality"][i:]) < threshold:
+            # If condition is met
             # import pdb; pdb.set_trace()
             trimmed_sequences.append(record[0:i])
+            trimmed += 1
             break  # stop the loop
-import pdb; pdb.set_trace()
+        elif i == (len(record.seq)-1):  # If end of sequence
+            trimmed_sequences.append(record[0:i])
+            not_trimmed += 1
 
-SeqIO.write(trimmed_sequences, "trimmed_seqs.fasta", "fasta")  ## write to file
 
 # Get statistics after trimming
 trimmed_seq_lengths = []
@@ -41,39 +57,38 @@ for record in trimmed_sequences:
     trimmed_seq_qualities.extend(record.letter_annotations["phred_quality"])
     # I use extend instead of append because ojbect is already list
 
-# Statistics
-print("untrimmed mean seq length;", numpy.mean(untrimmed_seq_lengths))
-print("trimmed mean seq length:", numpy.mean(trimmed_seq_lengths))
-print("untrimmed mean quality:", numpy.mean(untrimmed_seq_qualities))
-print("trimmed mean quality:", numpy.mean(trimmed_seq_qualities))
+# Statistical output
+print("Nr of seqs trimmed:", trimmed)
+print("Nr of seqs not_trimmed:", not_trimmed)
+print("Mean seq length before trimming:", numpy.mean(untrimmed_seq_lengths))
+print("Mean seq length after trimming:", numpy.mean(trimmed_seq_lengths))
+print("Mean quality before trimming:", numpy.mean(untrimmed_seq_qualities))
+print("Mean quality after trimming:", numpy.mean(trimmed_seq_qualities))
 #print("max quality:", (max(seq_qualities)))
 #print("min quality:", (min(seq_qualities)))
 
 # Plots
 plt.figure(1)
-plt.hist(untrimmed_seq_lengths)
-plt.title("Untrimmed Seq length histogram")
+plt.hist(untrimmed_seq_lengths, alpha=0.5, label="untrimmed", color="blue")
+plt.hist(trimmed_seq_lengths, alpha=0.5, label="trimmed", color="green")
+plt.legend(loc='upper right')
+plt.title("Seq length histogram")
 plt.xlabel("Value")
 plt.ylabel("Frequency")
-plt.savefig("../results/untrimmed_seqlength.pdf")
+
+file_name = "seqlength.pdf"
+plt.savefig(os.path.join(args.output_dir, file_name))  # save to specified dir
+print ("Plot seqlength.pdf saved")
 
 plt.figure(2)
-plt.hist(untrimmed_seq_qualities)
-plt.title("Untrimmed Seq quality histogram")
+plt.hist(untrimmed_seq_qualities, alpha=0.5, label="untrimmed", color="blue")
+plt.hist(trimmed_seq_qualities, alpha=0.5, label="trimmed", color="green" )
+plt.title("Seq quality histogram")
 plt.xlabel("Value")
 plt.ylabel("Frequency")
-plt.savefig("../results/untrimmed_seqquality.pdf")
+file_name = "seqquality.pdf"
+plt.savefig(os.path.join(args.output_dir, file_name))  # save to specified dir
+print ("Plot seqquality.pdf saved")
 
-plt.figure(3)
-plt.hist(trimmed_seq_lengths)
-plt.title("Trimmed Seq length histogram")
-plt.xlabel("Value")
-plt.ylabel("Frequency")
-plt.savefig("../results/trimmed_seqlength.pdf")
-
-plt.figure(4)
-plt.hist(trimmed_seq_qualities)
-plt.title("Trimmed Seq quality histogram")
-plt.xlabel("Value")
-plt.ylabel("Frequency")
-plt.savefig("../results/trimmed_seqquality.pdf")
+file_name = "trimmed_seqs.fastq"
+SeqIO.write(trimmed_sequences, os.path.join(args.output_dir, file_name), "fastq")  ## write to file
