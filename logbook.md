e4e5ba8744a698f0eae77188a2bba2feaa3819ed
Arvin C <arvin.chireh@gmail.com>
2017-12-30 12:03:34 +0100

    last commit before abandoning tr2 idea

--- logbook.md
+++ logbook.md
@@ -15,33 +15,73 @@ gunzip *
 * See trimming1.py.
 * Script now returns some general statistics and histograms, can be used to test effects of algorithm.
 
-# 2017-12-25 Ideas for algorithm
-* Break after the quality has been under 1 SD in two consecutive calls?
-* Break when the remaining bases have low quality (i.e. under x SD of mean)?
 
 # 2017-12-27 Re-shape the program - iterate twice
 * Originally, I intended to parse through the fastq file once and do the trimming instantly. However, I now want to iterate once first to get some statistics, i.e. mean and sd, which I will use for the subsequent trimming (trimming will require additional iteration). Therefore, I decided to store all records in a list first, and then iterate over the list as many times as necessary.
 
-# 2017-12-27 Evaluation of first algorithm on test and real data
-* My impression is that the current algorithm trims many reads but improves quality only marginally.
+# 2017-12-27 Algorithm 1
+* Takes the 30 first calls and calculates the mean and sd.
+* Why 30? Should be enough calls to give an accurate representation of the quality, before the technical quality decline have started.
+* Algorithm 1 iterates over each base and checks the mean of the remaining bases from i and onwards. If the mean is lower than  standard devation from the mean of the first 50 bases, the sequence will be cut at that point.
+* Why 1 standard deviation? Played around with this, and settled on 1 sd.
+
+# 2017-12-27 Evaluation of algorithm 1 on test and real data
+* See below. My impression trims pretty carefully, giving only slight increases in mean quality.
 
 ## Miseq1
-Nr of seqs trimmed: 22
-Nr of seqs not_trimmed: 3
+Nr of seqs trimmed: 19
+Nr of seqs not_trimmed: 6
 Mean seq length before trimming: 245.8
-Mean seq length after trimming: 176.48
+Mean seq length after trimming: 206.12
 Mean quality before trimming: 17.1938161107
-Mean quality after trimming: 18.2554397099
+Mean quality after trimming: 17.8461090627
 
 ## Miseq2
-Nr of seqs trimmed: 121838
-Nr of seqs not_trimmed: 128162
+Nr of seqs trimmed: 104960
+Nr of seqs not_trimmed: 145040
 Mean seq length before trimming: 168.813372
-Mean seq length after trimming: 101.328744
+Mean seq length after trimming: 148.828088
 Mean quality before trimming: 29.3656623126
-Mean quality after trimming: 34.9965331061
+Mean quality after trimming: 29.9118627124
 
-# 2017-12-27 Improve speed TO-DO
-* Remove one iteration (baked it in previous iteration) to make program faster.
+# 2017-12-27 Improve speed?
+* The program takes some time when run on the large file.
+* One way to make faster is to put the second and third (statistics) iterations together.
+* This would improve speed, but make the script more difficult to understand.
+* For now, I choose to not improve speed.
 
-# 2017-12-27 Use argparse to simplify usage
+# 2017-12-27 Added argparse to simplify program usage
+* See script.
+
+# 2017-12-27 Algorithm 2
+* First algorithm produced low yield. One disadvantage with the previous method is that all bases will be cut once the mean of the remaining bases is below the threshold, however, many of those bases that were cut could be even before the base call quality actually drops.
+* Maybe it's better to compare the 5 previous bases with the next 5 (or something similar)
+
+# 2017-12-28 Evaluation of Algorithm 2
+* Algorithm 2 doesn't work on Miseq2 data, reduces quality.
+
+## Miseq1
+Nr of seqs trimmed: 21
+Nr of seqs not_trimmed: 4
+Mean seq length before trimming: 245.8
+Mean seq length after trimming: 162.76
+Mean quality before trimming: 17.1938161107
+Mean quality after trimming: 18.216023593
+
+## Miseq2
+Nr of seqs trimmed: 157667
+Nr of seqs not_trimmed: 92333
+Mean seq length before trimming: 168.813372
+Mean seq length after trimming: 86.195444
+Mean quality before trimming: 29.3656623126
+Mean quality after trimming: 29.0669878561
+
+# 2017-12-30 New take on algorithm 2
+* I will abandon the idea of looking at consecutive bases. Instead, i will work further on algorithm 1, but with some changes.
+* To avoid premature trimming, I will start looking from the last 20 bases, and cut if the mean quality of the remaining bases is too low.
+
+# 2017-12-28 Adding controls
+* Empty file will produce warning. Tested and works.
+* Fasta file as input (instead of fastq) will give same error message. Tested and works.
+* File with short sequences: Short seqs will not be trimmed (but stored as is). Warning will be written to stdout. Tested and works.
+* Unambigious nucletoides are not considered. Going through each base and check it would make program less efficient, and not really the purpose either. Include in discussion.
---
+++ scripts/trimming.py
@@ -0,0 +1,167 @@
+#! /usr/bin/env python
+
+import argparse
+from Bio import SeqIO
+import matplotlib.pyplot as plt
+import numpy
+import os
+import sys
+
+parser = argparse.ArgumentParser(description='takes fastq files as input, \
+                                 trims reads, gives statistics and plots')
+parser.add_argument('algorithm',
+                    help='which algorithm to use, e.g. tr1 or tr2')
+parser.add_argument('fastq_file',
+                    help='path to fastq file, e.g. "miseq1.fq"')
+parser.add_argument('output_dir',
+                    help='Save plots to this directory, e.g. "./Results"')
+args = parser.parse_args()
+
+# Trim and Store
+trimmed_sequences = []
+with open(args.fastq_file, "rU") as fq:
+    records = list(SeqIO.parse(fq, "fastq"))
+
+# Control input file
+if not records:
+    sys.exit("Error: Empty file or wrong filetype. Script halted.")
+
+
+# Get length and quality (for downstream statistics)
+untrimmed_seq_lengths = []
+untrimmed_seq_qualities = []
+for record in records:
+    untrimmed_seq_lengths.append(len(record.seq))
+    untrimmed_seq_qualities.extend(record.letter_annotations["phred_quality"])
+
+
+# Do trimming with algorithm 1 or 2 depending on user input
+if args.algorithm == "tr1":  # Algorithm 1
+    trimmed = 0  # for keeping counts
+    not_trimmed = 0  # for keeping counts
+    trimmed_seq_lengths = []  # for downstream statistics
+    trimmed_seq_qualities = []  # for downsteam statistics
+    short_sequences = 0  # for control
+    for record in records:  # Parse through each accession
+        if len(record.seq) < 30:
+            short_sequences += 1  # Warning comes further down
+            trimmed_sequences.append(record)  # store w/o trimming
+        else:
+            # Calculate threshold
+            mean_quality = numpy.mean(
+                record.letter_annotations["phred_quality"][0:30])
+            std_quality = numpy.std(
+                record.letter_annotations["phred_quality"][0:30])
+            threshold = mean_quality - (1 * std_quality)
+            for i in range(0, len(record.seq)):  # Iterate over each base
+                # Check quality threshold and trim
+                if numpy.mean(
+                     record.letter_annotations["phred_quality"][i:]
+                     ) < threshold:
+                    trimmed_sequences.append(record[0:i])  # up to i:th base
+                    trimmed += 1
+                    break  # stop the loop
+                elif i == (len(record.seq)-1):  # If end of sequence
+                    trimmed_sequences.append(record)  # keep whole record
+                    not_trimmed += 1
+elif args.algorithm == "tr2":  # Algorithm 2
+    trimmed = 0  # for keeping counts
+    not_trimmed = 0  # for keeping counts
+    trimmed_seq_lengths = []  # for downstream statistics
+    trimmed_seq_qualities = []  # for downsteam statistics
+    short_sequences = 0  # for control
+    for record in records:  # Parse through each accession
+        if len(record.seq) < 30:
+            short_sequences += 1  # Warning comes further down
+            trimmed_sequences.append(record)  # store w/o trimming
+        else:
+            # Calculate metrics
+            mean_quality = numpy.mean(
+                record.letter_annotations["phred_quality"][0:50])
+            std_quality = numpy.std(
+                record.letter_annotations["phred_quality"][0:50])
+            threshold = mean_quality - (1 * std_quality)
+            for i in range(0, (len(record.seq)-4)):  # Iterate over each base
+                a = record.letter_annotations["phred_quality"][i]
+                b = record.letter_annotations["phred_quality"][i+1]
+                c = record.letter_annotations["phred_quality"][i+2]
+                d = record.letter_annotations["phred_quality"][i+3]
+                e = record.letter_annotations["phred_quality"][i+4]
+                five_consecutive = [a, b, c, d, e]
+                if all(q < threshold for q in five_consecutive):
+                    # If condition is met
+                    trimmed_sequences.append(record[0:i])  # up to i:th base
+                    trimmed += 1
+                    break  # stop the loop
+                elif i == (len(record.seq)-5):  # If end of sequence
+                    trimmed_sequences.append(record)  # keep whole record
+                    not_trimmed += 1
+
+# Warnings
+if short_sequences > 0:
+    print("Warning, ", short_sequences,
+          "sequences were less than 30 bp and not trimmed")
+
+# Write to file
+file_name = "trimmed_seqs.fastq"
+SeqIO.write(trimmed_sequences, os.path.join(args.output_dir, file_name),
+            "fastq")
+
+# Get statistics after trimming
+trimmed_seq_lengths = []
+trimmed_seq_qualities = []
+for record in trimmed_sequences:
+    trimmed_seq_lengths.append(len(record.seq))
+    trimmed_seq_qualities.extend(record.letter_annotations["phred_quality"])
+    # I use extend instead of append because ojbect is already list
+
+# Statistical output
+print("Nr of seqs trimmed:", trimmed)
+print("Nr of seqs not_trimmed:", not_trimmed+short_sequences)
+print("Mean seq length before trimming:", numpy.mean(untrimmed_seq_lengths))
+if trimmed:  # if there are any trimmed reads
+    print("Mean seq length after trimming:", numpy.mean(trimmed_seq_lengths))
+else:
+    print("Mean seq length after trimming: N/A. No reads trimmed")
+print("Mean quality before trimming:", numpy.mean(untrimmed_seq_qualities))
+if trimmed:  # if there are any trimmed reads
+    print("Mean quality after trimming:", numpy.mean(trimmed_seq_qualities))
+else:
+    print("Mean seq length after trimming: N/A. No reads trimmed")
+
+# Plots
+if trimmed:
+    plt.figure(1)
+    plt.hist(untrimmed_seq_lengths,
+             alpha=0.5,
+             label="untrimmed",
+             color="blue")
+    plt.hist(trimmed_seq_lengths,
+             alpha=0.5,
+             label="trimmed",
+             color="green")
+    plt.legend(loc='upper right')
+    plt.title("Seq length histogram")
+    plt.xlabel("Value")
+    plt.ylabel("Frequency")
+    file_name = "seqlength.pdf"
+    plt.savefig(os.path.join(args.output_dir, file_name))  # save to output_dir
+
+    plt.figure(2)
+    plt.hist(untrimmed_seq_qualities,
+             alpha=0.5,
+             label="untrimmed",
+             color="blue")
+    plt.hist(trimmed_seq_qualities,
+             alpha=0.5,
+             label="trimmed",
+             color="green")
+    plt.legend(loc='upper right')
+    plt.title("Seq quality histogram")
+    plt.xlabel("Value")
+    plt.ylabel("Frequency")
+    file_name = "seqquality.pdf"
+    plt.savefig(os.path.join(args.output_dir, file_name))  # save to output_dir
+    print("Plots saved in specified directory")
+else:
+    print("No plots were produced, as no reads were trimmed")
